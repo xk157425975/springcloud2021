e16d46062b5ab73ff20b37f0202be88cbb231f21
xukun <157425975@qq.com>
2021-06-28 21:11:48 +0800

    开发hystrix

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/xk/springcloud/service/impl/PaymentFallbackService.java
@@ -0,0 +1,18 @@
+package com.xk.springcloud.service.impl;
+
+import com.xk.springcloud.service.PaymentHystrixService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentFallbackService implements PaymentHystrixService {
+
+    @Override
+    public String paymentInfo_OK(Integer id) {
+        return "-----PaymentFallbackService fall back-paymentInfo_OK ,o(╥﹏╥)o";
+    }
+
+    @Override
+    public String paymentInfo_Timeout(Integer id) {
+        return "-----PaymentFallbackService fall back-paymentInfo_Timeout ,o(╥﹏╥)o";
+    }
+}
